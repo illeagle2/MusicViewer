49432bf44f2bd22404a0a3ad0d004f0b3cd25c47
illeagle2 <mdilleagle@gmail.com>
2023-01-03 10:22:57 -0500

    implemented recycler views inside each fragment

--- app/src/main/java/com/example/musicviewer/view/ClassicFragment.kt
+++ app/src/main/java/com/example/musicviewer/view/ClassicFragment.kt
@@ -1,7 +1,50 @@
 package com.example.musicviewer.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.musicviewer.R
+import com.example.musicviewer.databinding.FragmentClassicBinding
+import com.example.musicviewer.databinding.FragmentRockBinding
+import com.example.musicviewer.model.MusicResponse
+import com.example.musicviewer.view.adapter.MusicAdapter
 
 class ClassicFragment: Fragment(R.layout.fragment_classic) {
+
+    private lateinit var binding: FragmentClassicBinding
+    private val adapter: MusicAdapter by lazy{
+        MusicAdapter(emptyList())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        binding = FragmentClassicBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        initViews()
+        getData()
+        return binding.root
+    }
+
+    private fun initViews() {
+        binding.rvClassicSongs.layoutManager = LinearLayoutManager(context)
+        binding.rvClassicSongs.adapter = adapter
+    }
+
+    private fun getData(){
+        val classicList = listOf(MusicResponse("first song"), MusicResponse("second song"))
+        adapter.updateDataSet(classicList)
+
+    }
 }
--- app/src/main/java/com/example/musicviewer/view/adapter/MusicAdapter.kt
+++ app/src/main/java/com/example/musicviewer/view/adapter/MusicAdapter.kt
@@ -4,27 +4,43 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.musicviewer.R
+import com.example.musicviewer.databinding.MusicItemBinding
 import com.example.musicviewer.model.MusicResponse
 
 class MusicAdapter(
     private var dataSet: List<MusicResponse>
-): RecyclerView.Adapter<TodoViewHolder>() {
+): RecyclerView.Adapter<MusicAdapter.MusicViewHolder>() {
 
     fun updateDataSet(newDataSet: List<MusicResponse>){
         dataSet = newDataSet
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
-        return TodoViewHolder(view)
+    class MusicViewHolder(private val binding: MusicItemBinding):
+        RecyclerView.ViewHolder(binding.root){
+
+            fun onBind(musicItem: MusicResponse){
+                binding.apply {
+                    tvTitle.text = musicItem.title
+                }
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicViewHolder {
+        return MusicViewHolder(
+            MusicItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
-    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        holder.musicTitle.text = todos[position].title
+    override fun onBindViewHolder(holder: MusicViewHolder, position: Int) {
+        holder.onBind(dataSet[position])
     }
 
     override fun getItemCount(): Int {
-        return todos.size
+        return dataSet.size
     }
 }
