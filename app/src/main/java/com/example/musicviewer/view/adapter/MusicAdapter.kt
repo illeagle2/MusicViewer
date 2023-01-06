b121c1b4a233adb8f60c0ddaaf52484da50a71f0
illeagle2 <mdilleagle@gmail.com>
2023-01-06 15:43:51 -0500

    Implemented on click to play song

--- app/src/main/java/com/example/musicviewer/view/adapter/MusicAdapter.kt
+++ app/src/main/java/com/example/musicviewer/view/adapter/MusicAdapter.kt
@@ -1,14 +1,22 @@
 package com.example.musicviewer.view.adapter
 
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.musicviewer.databinding.MusicItemBinding
 import com.example.musicviewer.model.MusicResponse
 import com.example.musicviewer.model.Song
+import com.example.musicviewer.view.ClassicFragment
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 class MusicAdapter : RecyclerView.Adapter<MusicAdapter.MusicViewHolder>() {
 
@@ -47,14 +55,24 @@ class MusicAdapter : RecyclerView.Adapter<MusicAdapter.MusicViewHolder>() {
 
 
     override fun onBindViewHolder(holder: MusicAdapter.MusicViewHolder, position: Int) {
+        val context = holder.itemView.context
+
         holder.binding.apply {
             val song = songs[position]
             tvTitle.text = song.artistName
             tvGroup.text = song.collectionName
             tvPrice.text = song.trackPrice.toString()
-            Picasso.get().load(song.artworkUrl60).resize(300,300).into(ivArtwork)
+            Picasso.get().load(song.artworkUrl100).resize(300,300).into(ivArtwork)
         }
+
+        holder.itemView.setOnClickListener{
+            val queryUrl: Uri = Uri.parse(songs[position].previewUrl)
+            val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+            context.startActivity(intent)
+            }
+
     }
+
 
     override fun getItemCount(): Int {
         return songs.size
